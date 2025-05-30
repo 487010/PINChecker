e87edaa767427b93a2468ddc4a2378714d99becd
487010 <487010@bsd48.org>
2025-05-30 16:16:52 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
+// Declare Variables
+let digit1 
+let digit2
+let digit3
+let digit4
+let input1
+let input2
+let input3
+let input4
+let characters 
 
+basic.showString("Please input a code")
+characters = 0
+while (characters < 4){
+    if (true) {
+        
+    }
+}
