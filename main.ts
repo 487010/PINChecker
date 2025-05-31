859b695a20c7b0b12e3905a09b0fa03d040aaefd
487010 <487010@bsd48.org>
2025-05-30 17:23:23 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -11,8 +11,22 @@ let characters
 
 basic.showString("Please input a code")
 characters = 0
-while (characters < 4){
-    if (true) {
-        
-    }
+while (characters < 4){ 
+    input.onButtonPressed(Button.A, function() {
+        if (characters = 0){
+            digit1 = "A"
+        }
+        else
+            if (characters = 1){
+                digit2 = "A"
+            }
+            else
+                if (characters = 2){
+                    digit3 = "A"
+                }
+                else
+                    if (characters = 3){
+                        digit4 = "A"
+                    }
+    })
 }
